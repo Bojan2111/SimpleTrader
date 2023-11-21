185388dc071894c94ffb4c704a562d0d21886d94
Bojan2111 <bojan.adzic@gmail.com>
2023-11-21 22:49:23 +0100

    Add Navigation bar and commands

---
+++ SimpleTrader.WPF/State/Navigators/Navigator.cs
@@ -0,0 +1,28 @@
+﻿using SimpleTrader.WPF.Commands;
+using SimpleTrader.WPF.Models;
+using SimpleTrader.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SimpleTrader.WPF.State.Navigators
+{
+    public class Navigator : ObservableObject, INavigator
+    {
+        private ViewModelBase _viewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get { return _viewModel; }
+            set
+            {
+                _viewModel = value;
+                OnPropertyChanged(nameof(CurrentViewModel));
+            }
+        }
+        public ICommand UpdateCurrentViewModelCommand => new UpdateCurrViewModelCommand(this);
+    }
+}

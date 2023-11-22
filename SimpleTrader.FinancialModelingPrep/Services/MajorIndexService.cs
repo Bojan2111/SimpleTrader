35f87b91407864c2d174b868a45de425703de762
Bojan2111 <bojan.adzic@gmail.com>
2023-11-22 20:26:51 +0100

    Add API Project and MajorIndexService

---
+++ SimpleTrader.FinancialModelingPrep/Services/MajorIndexService.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using SimpleTrader.Domain.Models;
+using SimpleTrader.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.FinancialModelingPrep.Services
+{
+    internal class MajorIndexService : IMajorIndexService
+    {
+        public async Task<MajorIndex> GetMajorIndex(MajorIndexType indexType)
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("https://financialmodelingprep.com//api/v3/majors-indexes/.DJI");
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                MajorIndex majorIndex = JsonConvert.DeserializeObject<MajorIndex>(jsonResponse);
+                return majorIndex;
+            }
+        }
+    }
+}
